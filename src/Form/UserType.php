088231c6f9ee2f27f9cc24b249f91d8d51fc129c
KenSerikawa <kenserikawalucio@gmail.com>
2020-09-02 00:47:48 +0200

    Add passwordType class

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -19,7 +20,12 @@ class UserType extends AbstractType
                     'class'=> ''
                 ]
             ])
-            ->add('password')
+            ->add('password', PasswordType::class, [
+                'required' => true, 
+                'attr' => [
+                    'class'=> ''
+                ]
+            ])
         ;
     }
 

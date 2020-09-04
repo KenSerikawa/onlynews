6924ce3f7be915682b52a53b26697507ca5510e6
KenSerikawa <kenserikawalucio@gmail.com>
2020-09-04 17:43:16 +0200

    Add path to user view when account is created

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -44,7 +44,7 @@ class UserController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
 
-            return $this->redirectToRoute('user_index');
+            return $this->redirectToRoute('user_show', ['id' => $user->getId()]);
         }
 
         return $this->render('user/new.html.twig', [

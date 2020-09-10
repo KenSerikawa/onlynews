01e6f221f0589d98471fea95f981f530d58b175d
KenSerikawa <kenserikawalucio@gmail.com>
2020-09-10 23:05:06 +0200

    Add source color toggle animation mouse in and out (hover)

--- public/js/onhover-article-animation.js
+++ public/js/onhover-article-animation.js
@@ -1,5 +1,4 @@
 function addBlur(element) {
-    console.log(element)
     if(!element.classList.contains('blur')) {
         element.classList.add('blur')
     }
@@ -9,6 +8,24 @@ function removeBlur(element) {
     if(element.classList.contains('blur')) {
         element.classList.remove('blur')
     }
+}
+
+function getSource(element) {
+    return element.children[0].children[0].children[6].children[0]
+}
+
+function highlightFontColor(element) {
+    if(element.classList.contains('text-danger')) {
+        element.classList.remove('text-danger')
+        element.classList.add('text-white')
+    } 
+}
+
+function adjustFontColor(element) {
+    if(element.classList.contains('text-white')) {
+        element.classList.remove('text-white')
+        element.classList.add('text-danger')
+    } 
 }
 
 function getAllArticles() {
@@ -21,17 +38,20 @@ function highlight(element) {
         if(article.id !== element.id) {
             addBlur(article)
         } else {
+            highlightFontColor(getSource(element))
             element.classList.add('flashing')
         }
     });
 }
 
-function readjust() {
+function readjust(element) {
     let articles = getAllArticles()
 
     articles.forEach(article => {
             removeBlur(article)
     });
+
+    adjustFontColor(getSource(element))
 }
 /* 
 document.addEventListener("DOMContentLoaded", function() {
